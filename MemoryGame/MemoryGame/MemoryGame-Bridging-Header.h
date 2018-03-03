44a026b6ed128904bb979133f0a9aa14c42c0042
Brian Xu <35032810+W952@users.noreply.github.com>
2018-03-03 20:29:52 +0800

    trash

---
+++ MemoryGame/MemoryGame/MemoryGame-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  MemoryGame-Bridging-Header.h
+//  MemoryGame
+//
+//  Created by Brian Xu on 27/2/2018.
+//  Copyright © 2018 Fake Name. All rights reserved.
+//
+
+#import <CouchbaseLite/CouchbaseLite.h>
+#import <CouchbaseLiteListener/CouchbaseLiteListener.h>
